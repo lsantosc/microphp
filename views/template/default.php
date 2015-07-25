a8218bbf620ef831baa1f05fc150e1ac69495a17
Lucas Santos da Costa <lsantosc@live.com>
2015-07-25 11:40:38 -0300

    Fix css path in default template

--- views/template/default.php
+++ views/template/default.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title><?PHP echo $this->e($title); ?></title>
-    <link rel="stylesheet" href="<?=$this->asset('css\reset.css')?>" />
+    <link rel="stylesheet" href="<?=$this->asset('css/reset.css')?>" />
 </head>
 <body>
     <?PHP echo $this->section('content'); ?>
